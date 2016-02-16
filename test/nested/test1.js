9e6ecad71c2b6f39c9c2f28ac1e85136f31cc695
jimmy <villainthr@gmail.com>
2016-02-16 21:00:04 +0800

    fixed

--- test/nested/test1.js
+++ test/nested/test1.js
@@ -2,25 +2,22 @@ var express = require("express");
 var request = require("supertest");
 var app = express();
 var expect = require('chai').expect;
+var bodyParser = require('body-parser');
 
 // 定义路由
-app.get('/user', function(req, res){
-  res.status(202).send({ name: 'jerryc' });
-});
+describe('POST /user', function(){
+  it('should work with .send() etc', function(done){
+    var app = express();
 
-describe('GET /user', function(){
-  it('respond with json', function(done){
+    app.use(bodyParser.json());
+
+    app.post('/', function(req, res){
+      res.send(req.body.name);
+    });
+
     request(app)
-      .get('/user')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err){
-          done(err);
-        }
-        expect(res.body.name).to.equal('jerryc');
-        done();
-      })
+    .post('/')
+    .send({ name: 'jimmy' })
+    .expect('jimmy', done);
   });
 });
