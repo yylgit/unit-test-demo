dc7f72d3b0daf35728f493cb823b02963d6d3ecc
杨益良 <yangyiliang@meituan.com>
2016-05-03 15:43:04 +0800

    test

--- test/test.js
+++ test/test.js
@@ -21,3 +21,33 @@ describe('POST /user', function(){
     .expect('jimmy', done);
   });
 });
+
+
+//自己测试的代码
+var Cal = (function(){
+    var num = {
+        base:0
+    };
+    var add = function(){
+        num.base++;
+        return num.base;
+    }
+    var desc = function(){
+        --num.base;
+        return num.base;
+    }
+    return {
+        add,desc,num
+    }
+})();
+//ok,现在引用断言库chai
+var expect = require('chai').expect;
+//写出测试
+describe('Counter',function(){
+    it('it should increase',function(){
+        expect(Cal.num.base).to.below(Cal.add());
+    })
+    it('it should decrease',function(){
+        expect(Cal.num.base).to.above(Cal.desc());
+    })
+})
